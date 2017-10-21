ea6bfa361b20717b0ac243557a3d017e65002bd1
mjoodsh <malshikhhussain@mail.csuhcio.edu>
2017-10-20 19:30:49 -0700

    Added if statment for the negative and zero not print

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+ 
+vector<vector<char> > x;
+
+
+int input1;
+int input2;
+
+int main() {
+
+cout<<"Enter rows and columns of stars:"<<endl;
+cin>>input1;
+cin>>input2;
+if (input1 >0 || input2 >0){
+
+x.resize(input1);
+
+
+     for (int i = 0; i <x.size(); i ++) {
+     	x[i].resize(input2);
+        
+        for (int j = 0; j <x[i].size(); j ++) {
+            x[i][j]='*';
+           
+            cout << "*";
+            
+        }
+        cout << endl;
+    }
+    }
+   
+return 0;
+}
